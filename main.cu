10b804f2ed9ed27b5b00c446c743df9af57c51d2
KenyaTamura <denn_sonn@yahoo.co.jp>
2017-09-14 15:13:55 +0900

    Not research part

---
+++ main.cu
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include"Data.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+/*
+	if (argc == 1) {
+		cout << "No entry\n";
+		return 0;
+	}
+	else{
+		// file read by Data class
+		Data data(argv[1]);
+	}
+*/
+	Data base(10000);	
+	Data query(20);
+	return 0;
+}
